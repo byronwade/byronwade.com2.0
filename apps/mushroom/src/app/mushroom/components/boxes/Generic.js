e305dd7f5ab6a574389d8836ef973d0a138edd9c
Byron Wade <bcw1995@gmail.com>
2023-12-22 08:48:54 -0800

    test

--- apps/mushroom/src/app/mushroom/components/boxes/Generic.js
+++ apps/mushroom/src/app/mushroom/components/boxes/Generic.js
@@ -1,9 +1,8 @@
 export default function Generic({ width }) {
   return (
     <div className={`w-${width.sm}/12 md:w-${width.md}/12 lg:w-${width.lg}/12`}>
-      <div className="relative h-full p-4 space-y-2 overflow-hidden border rounded-md shadow-lg border-neutral-800 bg-neutral-900">
-        Substrate:{' '}
-        <span className="block text-sm text-gray-600 dark:text-gray-300">Hardwoods, sawdust</span>
+      <div className="relative h-full p-4 space-y-2 overflow-hidden text-white border rounded-md shadow-lg border-neutral-800 bg-neutral-900">
+        Substrate: <span className="block text-sm text-gray-300">Hardwoods, sawdust</span>
       </div>
     </div>
   );
