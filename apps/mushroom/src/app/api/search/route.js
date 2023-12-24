ce052d91d64c569fec46ee63036cf36c0d40b870
Byron Wade <bcw1995@gmail.com>
2023-12-23 18:06:39 -0800

    test

--- apps/mushroom/src/app/api/search/route.js
+++ apps/mushroom/src/app/api/search/route.js
@@ -2,8 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY);
 
 export const runtime = 'edge';
-// Prevents this route's response from being cached
-export const dynamic = 'force-dynamic';
 
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
@@ -12,28 +10,44 @@ export async function GET(req) {
 
   let responseStream = new ReadableStream({
     async start(controller) {
-      let data, error;
+      try {
+        let totalRecords = 0;
+        let loadedRecords = 0;
 
-      if (slug) {
-        // If a slug is provided, use it for an exact match
-        ({ data, error } = await supabase.from('mushrooms').select('*').eq('slug', slug).single());
-      } else if (query) {
-        // If a query is provided, perform a text search
-        ({ data, error } = await supabase
-          .from('mushrooms')
-          .select('*')
-          .textSearch('search_text', query, { type: 'websearch' })
-          .limit(20));
-      } else {
-        // Handle case where neither slug nor query is provided
-        error = { message: 'No search parameters provided.' };
+        // Dummy function to simulate chunked data loading
+        const loadChunk = async (offset, limit) => {
+          const { data, error } = await supabase
+            .from('mushrooms')
+            .select('*', { count: 'exact', head: true })
+            .range(offset, offset + limit - 1);
+
+          if (error) throw error;
+          return data;
+        };
+
+        // Get total number of records for progress calculation
+        if (query) {
+          const { count } = await supabase
+            .from('mushrooms')
+            .select('*', { count: 'exact', head: true })
+            .textSearch('search_text', query, { type: 'websearch' });
+
+          totalRecords = count;
+        }
+
+        // Load data in chunks and send progress updates
+        while (loadedRecords < totalRecords) {
+          const chunkSize = 20; // Define your chunk size
+          const dataChunk = await loadChunk(loadedRecords, chunkSize);
+          loadedRecords += dataChunk.length;
+
+          const progress = (loadedRecords / totalRecords) * 100;
+          controller.enqueue(JSON.stringify({ progress, data: dataChunk }));
+        }
+      } catch (error) {
+        controller.enqueue(JSON.stringify({ error: error.message }));
       }
 
-      if (error) {
-        controller.enqueue(JSON.stringify({ error }));
-      } else {
-        controller.enqueue(JSON.stringify({ data }));
-      }
       controller.close();
     }
   });
